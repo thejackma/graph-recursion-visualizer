1b97826deeb328165530c749eaf7643ec375bac2
Jack Ma <guoyaoma66@gmail.com>
2022-07-20 00:36:10 -0400

    Save code

--- js/editor.js
+++ js/editor.js
@@ -1,8 +1,19 @@
 import * as monaco from 'https://cdn.jsdelivr.net/npm/monaco-editor@0.33/+esm';
 import { defaultCodeExample, fetchCodeExample } from './code-example.js'
 
+const configKeyCode = 'code';
+
+let code = localStorage.getItem(configKeyCode);
+if (!code) {
+    code = await fetchCodeExample(defaultCodeExample);
+}
+
 export const editor = monaco.editor.create(document.getElementById('editor'), {
-    value: await fetchCodeExample(defaultCodeExample),
+    value: code,
     language: 'javascript',
     automaticLayout: true,
+});
+
+addEventListener('beforeunload', e => {
+    localStorage.setItem(configKeyCode, editor.getValue());
 });
