ca18123e35eda124af2c5231750abf7952cd3a42
Jack Ma <guoyaoma66@gmail.com>
2022-07-23 02:40:06 -0400

    Top-down stack

--- js/stack.js
+++ js/stack.js
@@ -2,7 +2,33 @@ export const stack = Vue.createApp({
     data() {
         return {
             stack: [],
+            topDown: true,
         };
+    },
+    computed: {
+        stackTopIndex() {
+            return this.topDown ? this.stack.length - 1 : 0;
+        },
+        stackView() {
+            if (this.topDown) {
+                return this.stack;
+            } else {
+                return this.stack.slice().reverse();
+            }
+        },
+        stackGrid() {
+            if (this.topDown) {
+                return {
+                    'grid-template-rows': 'auto 1fr',
+                    'grid-template-areas': '"frames" "empty"',
+                };
+            } else {
+                return {
+                    'grid-template-rows': '1fr auto',
+                    'grid-template-areas': '"empty" "frames"',
+                };
+            }
+        },
     },
     methods: {
         reset() {
@@ -23,3 +49,16 @@ export const stack = Vue.createApp({
         },
     },
 }).mount('#stack');
+
+const stackControls = Vue.createApp({
+    data() {
+        return {
+            stackMode: stack.topDown ? 'TopDown' : 'BottomUp',
+        };
+    },
+    watch: {
+        stackMode(newValue, oldValue) {
+            stack.topDown = newValue === 'TopDown';
+        },
+    },
+}).mount('#stack-controls');
